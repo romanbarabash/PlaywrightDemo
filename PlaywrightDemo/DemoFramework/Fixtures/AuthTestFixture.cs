134f17bab72545a07de5ef2600a0d6c9ce523387
rbarabash <roman.barabash@materialise.kiev.ua>
2024-11-28 14:18:10 +0200

    add trace for AuthTestFixture

--- PlaywrightDemo/DemoFramework/Fixtures/AuthTestFixture.cs
+++ PlaywrightDemo/DemoFramework/Fixtures/AuthTestFixture.cs
@@ -4,34 +4,71 @@ namespace PlaywrightDemo.POM.Fixtures;
 
 public class AuthTestFixture
 {
-    public IBrowser Browser { get; set; }
-    public IPage Page { get; set; }
+    public static IPlaywright Playwright { get; private set; }
+    public static IBrowser Browser { get; private set; }
+    public IBrowserContext Context { get; private set; }
+    public IPage Page { get; private set; }
+
+    [OneTimeSetUp]
+    public async Task OneTimeSetup()
+    {
+        Playwright = await Microsoft.Playwright.Playwright.CreateAsync();
+        Browser = await Playwright.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
+        {
+            Headless = false,
+            Args = new[] { "--start-maximized" }
+        });
+    }
 
     [SetUp]
     public async Task Setup()
     {
-        var PW = await Playwright.CreateAsync();
-        Browser = await PW.Chromium.LaunchAsync(new BrowserTypeLaunchOptions
-        {
-            Headless = false,
-            Args = ["--start-maximized"]
-        });
-
-        // create new context from saved state
-        var context = await Browser.NewContextAsync(new()
+        // Create new context from saved state
+        Context = await Browser.NewContextAsync(new BrowserNewContextOptions
         {
             StorageStatePath = Path.Combine(Directory.GetCurrentDirectory(), "state.json"),
-            ViewportSize = ViewportSize.NoViewport
+            ViewportSize = new ViewportSize { Width = 1920, Height = 1080 }
         });
 
-        Page = await context.NewPageAsync();
-
+        Page = await Context.NewPageAsync();
+        await StartTraceTests(Context);
     }
 
     [TearDown]
     public async Task TearDown()
     {
+        await StopTraceTests(Context);
         await Page.CloseAsync();
+        await Context.CloseAsync();
+    }
+
+    [OneTimeTearDown]
+    public async Task OneTimeTearDown()
+    {
         await Browser.CloseAsync();
+        Playwright.Dispose();
+    }
+
+    public async Task StartTraceTests(IBrowserContext context)
+    {
+        await context.Tracing.StartAsync(new TracingStartOptions
+        {
+            Title = $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}",
+            Screenshots = true,
+            Snapshots = true,
+            Sources = true
+        });
+    }
+
+    public async Task StopTraceTests(IBrowserContext context)
+    {
+        await context.Tracing.StopAsync(new TracingStopOptions
+        {
+            Path = Path.Combine(
+                TestContext.CurrentContext.WorkDirectory,
+                "playwright-traces",
+                $"{TestContext.CurrentContext.Test.ClassName}.{TestContext.CurrentContext.Test.Name}.zip"
+            )
+        });
     }
 }
