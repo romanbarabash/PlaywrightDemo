c6a65036d7829ebde478e73743fea2f4f48b123d
rbarabash <roman.barabash@materialise.kiev.ua>
2024-11-29 14:53:45 +0100

    add UploadFileTest

---
+++ PlaywrightDemo/DemoFramework/Pages/UploadFilePage.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.Playwright;
+
+namespace PlaywrightDemo.DemoFramework.Pages
+{
+    public class UploadFilePage
+    {
+        private readonly IPage _page;
+
+        public UploadFilePage(IPage page)
+        {
+            _page = page;
+        }
+
+        private ILocator choseFile => _page.Locator("input[type=\"file\"]");
+        private ILocator submit => _page.Locator("button[type=\"submit\"]");
+
+        public async Task UploadFile(string filePath)
+        {
+            await choseFile.SetInputFilesAsync(filePath);
+        }
+
+        public async Task ClickSubmit()
+        {
+            await submit.ClickAsync();
+        }
+
+        public async Task GoTo()
+        {
+            await _page.GotoAsync("https://commitquality.com/practice-file-upload");
+        }
+    }
+}
+
---
+++ PlaywrightDemo/DemoFramework/Tests/UploadFilesTest.cs
@@ -0,0 +1,35 @@
+﻿using PlaywrightDemo.DemoFramework.Pages;
+using PlaywrightDemo.POM.Fixtures;
+
+namespace PlaywrightDemo.DemoFramework.Tests;
+
+
+[TestFixture]
+class UploadFilesTest : BaseTest
+{
+    private UploadFilePage _uploadFilePage;
+
+    [SetUp]
+    public void SetUp()
+    {
+        _uploadFilePage = new UploadFilePage(Page);
+    }
+
+    [Test]
+    public async Task UploadFile()
+    {
+        await _uploadFilePage.GoTo();
+        await Page.PauseAsync();
+
+        await _uploadFilePage.UploadFile(Path.Combine(AppContext.BaseDirectory, "..", "..", "..", "DemoFramework", "TestArtifacts", "README.txt"));
+
+        // Add event listener for the dialog box
+        Page.Dialog += async (_, dialog) =>
+        {
+            await Page.PauseAsync();
+            await dialog.AcceptAsync();
+        };
+        await Page.PauseAsync();
+        await _uploadFilePage.ClickSubmit();
+    }
+}
